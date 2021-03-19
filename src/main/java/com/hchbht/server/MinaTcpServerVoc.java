3944fb42f063822725ae725cc0d7901e2bbfe576
zhuzh <1135902780@qq.com>
2021-03-19 18:09:28 +0800

    预警数据接口

--- src/main/java/com/hchbht/server/MinaTcpServerVoc.java
+++ src/main/java/com/hchbht/server/MinaTcpServerVoc.java
@@ -7,7 +7,9 @@ import org.apache.mina.core.session.IoSession;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 /**
  * Created by zzh on 2021/03/17
@@ -32,13 +34,34 @@ public class MinaTcpServerVoc extends IoHandlerAdapter {
         System.out.println("设备IP---------- -->"  + clientIP);
         System.out.println("设备PORT---------- -->"  + clientPort);
         System.out.println("VOC--------------VOC数据Message -->"  + str);
-        if (str.trim().equalsIgnoreCase("quit")) {
-            session.close();
-            return;
-        }
-        Date date = new Date();
-        // 返回数据
+        List<String> list = new ArrayList<>();
+        str = "##0340ST=31;CN=2011;PW=123456;MN=88888880000009;Flag=0;CP=&&DataTime=20151218092100;S101-Rtd=10.5,S101-Flag=N;S102-Rtd=0,S102-Flag=N;S103-Rtd=0.5,S103-Flag=N;S104-Rtd=0,S104-Flag=N;S105-Rtd=1.728,S105-Flag=N;S201-Rtd=24.31,S201-Flag=N;S202-Rtd=0,S202-Flag=N;H101-Rtd=1,H101-Flag=N;H102-Rtd=0,H102-Rtd=N;J01-Rtd=1,J02-Flag=N;J02-Rtd=1,J02-Flag=N&&1B00";
+        if(StringUtils.isNotBlank(str) && str.length()>=6 && str.startsWith("##")){
+            String firstData = str.substring(6,str.length()-6);
+            String[] dataList = firstData.split(";");
+            for (int i = 0; i < dataList.length; i++) {
+                if(dataList[i].contains("&&")){
+                    list.add(dataList[i].split("&&")[1]);
+                }else{
+                    String[] data =  dataList[i].split(",");
+                    for (int j = 0; j < data.length; j++) {
+                        list.add(data[j]);
+                    }
+                }
+                if(!list.isEmpty()){
+                    list.parallelStream().forEach(res -> {
+
+                    });
+                }
+            }
+            if (str.trim().equalsIgnoreCase("quit")) {
+                session.close();
+                return;
+            }
+            Date date = new Date();
+            // 返回数据
 //        session.write(date.toString());
+        }
     }
     @Override
     public void sessionClosed(IoSession session) throws Exception {
